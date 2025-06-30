e6b9000a009e32c498316e48e0bf028c2baae14c
xia huang <xiasu@MacBook-Air-4.local>
2025-06-30 18:04:59 -0400

    add urls

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for django_portfolio project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,16 +8,8 @@ urlpatterns = [
     path('', include('main.urls')),
 ]
 
-
-
-# Serve static files during development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Serve static files in production (for platforms that don't handle it automatically)
-if not settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# Serve media files in production (safe with Render Disk)
+# Always serve media files (safe with Render Disk)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
