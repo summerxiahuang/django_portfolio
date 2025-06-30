7de8546df6ba1dd628f67e3c65489f21ccdb6cca
xia huang <xiasu@MacBook-Air-4.local>
2025-06-30 16:27:32 -0400

    remove key

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -21,15 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o7$x4f+nlr5q43qw52f+8oh3&y!@eg16#&d_-!3(ns1&0_waw6'
-
+# 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-default')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
 
 #ALLOWED_HOSTS = ['.elasticbeanstalk.com', '127.0.0.1','localhost']
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
-ALLOWED_HOSTS += ['.elasticbeanstalk.com', '127.0.0.1', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
+# ALLOWED_HOSTS += ['.elasticbeanstalk.com', '127.0.0.1', 'localhost', '0.0.0.0']
 # Application definition
 
 INSTALLED_APPS = [
